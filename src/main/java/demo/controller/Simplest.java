fca1ee1d0c52680b0f498fab75c8f7f30027b7c3
Fangshuo Pei(IT) <fangshuo.pei@cicc.com.cn>
2020-06-12 15:16:45 +0800

    spring session + redis operations session repository

--- src/main/java/demo/controller/Simplest.java
+++ src/main/java/demo/controller/Simplest.java
@@ -1,13 +1,50 @@
 package demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.FindByIndexNameSessionRepository;
+import org.springframework.session.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+@RestController
 public class Simplest {
-    @RequestMapping("/simplest")
-    public String particle() {
-        System.out.println("simplest........");
-        return "simplest";
+
+    @Autowired
+    FindByIndexNameSessionRepository<? extends Session> sessionRepository;
+
+    @RequestMapping("/test/cookie")
+    public String cookie(@RequestParam("browser") String browser, HttpServletRequest request, HttpSession session) {
+        //取出session中的browser
+        Object sessionBrowser = session.getAttribute("browser");
+        if (sessionBrowser == null) {
+            System.out.println("不存在session，设置browser=" + browser);
+            System.out.println("sessionId : " + session.getId());
+            session.setAttribute("browser", browser);
+        } else {
+            System.out.println("存在session，browser=" + sessionBrowser.toString());
+            System.out.println("sessionId : " + session.getId());
+        }
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getName() + " : " + cookie.getValue());
+            }
+        }
+        return "index";
+    }
+
+    @RequestMapping("/test/findByUsername")
+    @ResponseBody
+    public Map findByUsername(@RequestParam String username) {
+        Map<String, ? extends Session> usersSessions = sessionRepository.findByIndexNameAndIndexValue(FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME, username);
+        return usersSessions;
     }
 }
